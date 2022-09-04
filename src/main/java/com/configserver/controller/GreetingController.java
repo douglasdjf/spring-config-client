ff36398e3b5b016db6b15812a169852007083e1e
Douglas Fonseca <douglas.vasco79@gmail.com>
2022-09-03 23:35:56 -0300

    first commit

---
+++ src/main/java/com/configserver/controller/GreetingController.java
@@ -0,0 +1,34 @@
+package com.configserver.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.configserver.config.GreetingConfig;
+import com.configserver.model.Greeting;
+
+@RestController
+public class GreetingController {
+
+	private static final String template = "%s, %s!";
+	private final AtomicLong counter = new AtomicLong();
+	
+	@Autowired
+	private GreetingConfig config;
+	
+	@RequestMapping("/greeting")
+	public Greeting greeting(
+			@RequestParam(value="name",
+			defaultValue = "") String name) {
+		
+		if(name.isEmpty()) name = config.getDefaultValue();
+		
+		return new Greeting(
+					counter.incrementAndGet(),
+					String.format(template, config.getGreeting(), name)
+				);
+	}
+}
